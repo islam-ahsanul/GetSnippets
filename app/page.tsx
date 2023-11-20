de645bd4e82662f39334c225c1867e982f453312
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-11-20 14:31:38 +0600

    component files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import Feed from '@/components/feed';
 
 const homepage = () => {
   return (
-    <section className="flex w-full items-center justify-center">
-      <h1>Homepage</h1>
+    <section className="flex w-full flex-col items-center justify-center">
+      <h1 className="head_text text-center text-foreground">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </h1>
+      <p className="desc muted-foreground text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus
+        error quae quod eligendi nemo veritatis nam delectus accusamus labore
+        tenetur fugiat, libero cupiditate asperiores explicabo minus odio est
+        eos?
+      </p>
+      <Feed />
     </section>
   );
 };
