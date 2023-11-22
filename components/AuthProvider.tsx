af58276e605b3aec0696d7d00b0770d7ecba2a5d
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-11-22 09:09:23 +0600

    added assets

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from 'next-auth/react';
 export default function AuthProvider({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode; // session: any;
 }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
