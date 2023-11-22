5791b5f4c37c811de77898c582e963b74518ce50
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-11-22 21:42:41 +0600

    form component

--- app/create-post/page.tsx
+++ app/create-post/page.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+'use client';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import Form from '@/components/Form';
+
+export type PostType = {
+  title: string;
+  body: string;
+  timestamp: string;
+};
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const [post, setPost] = useState<PostType>({
+    title: '',
+    body: '',
+    timestamp: '',
+  });
+  const createThePost = (): void => {
+    // Add your code here
+  };
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createThePost}
+    />
+  );
 };
 
 export default CreatePost;
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,7 +1,77 @@
-import React from 'react';
+import Link from 'next/link';
 
-const Form = () => {
-  return <div>Form</div>;
+import { PostType } from '@/app/create-post/page';
+
+type FormProps = {
+  type: string;
+  post: PostType;
+  setPost: React.Dispatch<React.SetStateAction<PostType>>;
+  submitting: boolean;
+  handleSubmit: () => void;
+};
+
+const Form = ({ type, post, setPost, submitting, handleSubmit }: FormProps) => {
+  return (
+    <section className="flex-start flex w-full max-w-full flex-col items-start justify-start">
+      <h1 className="head_text text-left">
+        <span className="text-blue-500">{type} Post</span>
+      </h1>
+      <p className="desc max-w-md text-left">
+        {type} and share your code with the world.
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="glassmorphism mt-10 flex w-full max-w-2xl flex-col gap-7"
+      >
+        <label>
+          <span className="text-base font-semibold text-red-500">
+            Your Code Title
+          </span>
+          <textarea
+            value={post.title}
+            onChange={(e) =>
+              setPost({
+                ...post,
+                title: e.target.value,
+              })
+            }
+            placeholder="Write your code title here..."
+            required
+            className="form_textarea"
+          ></textarea>
+        </label>
+        <label>
+          <span className="text-base font-semibold text-red-500">
+            Your Code
+          </span>
+          <textarea
+            value={post.body}
+            onChange={(e) =>
+              setPost({
+                ...post,
+                body: e.target.value,
+              })
+            }
+            placeholder="Write your code here..."
+            required
+            className="form_textarea"
+          ></textarea>
+        </label>
+        <div className="mx-3 mb-5 flex items-center justify-end gap-4">
+          <Link href="/" className="text-sm text-gray-500">
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            disabled={submitting}
+            className="rounded-full bg-yellow-500 px-5 py-1.5 text-sm text-white"
+          >
+            {submitting ? `'${type}...` : type}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default Form;
