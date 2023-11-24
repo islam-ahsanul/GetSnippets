00bc64845317a4b57c56d6e9b214c783b50892e9
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-11-24 18:52:57 +0600

    landing text

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,14 @@ import Feed from '@/components/Feed';
 
 const homepage = () => {
   return (
-    <section className="flex w-full flex-col items-center justify-center">
-      <h1 className="head_text text-center text-foreground">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+    <section className="mx-10 flex flex-col items-center justify-center">
+      <h1 className="mt-3 text-center text-2xl font-extrabold leading-[1.15] text-foreground sm:text-5xl">
+        Share, Innovate, Inspire with GetSnippets
       </h1>
-      <p className="desc text-center text-muted-foreground">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus
-        error quae quod eligendi nemo veritatis nam delectus accusamus labore
-        tenetur fugiat, libero cupiditate asperiores explicabo minus odio est
-        eos?
+      <p className="mt-5 max-w-[900px] text-center text-lg text-muted-foreground sm:text-xl">
+        Join a passionate community of developers driving innovation through
+        shared snippets. Explore, collaborate, and grow together in an ecosystem
+        fueled by collective knowledge.
       </p>
       <Feed />
     </section>
